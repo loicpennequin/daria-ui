75c755ea7aed6b86fb0bd9a16a291e001505a170
loicpennequin <lo.pennequin@gmail.com>
2022-09-28 00:47:33 +0200

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
+  ],
+  overrides: [
+    {
+      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    curly: ["error", "multi"],
+    "vue/no-multiple-template-root": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error", { typedefs: false }],
+    "@typescript-eslint/no-explicit-any": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/attributes-order": [
+      "warn",
+      {
+        order: [
+          "DEFINITION",
+          "LIST_RENDERING",
+          "CONDITIONALS",
+          "RENDER_MODIFIERS",
+          "TWO_WAY_BINDING",
+          "GLOBAL",
+          ["UNIQUE", "SLOT"],
+          "OTHER_DIRECTIVES",
+          "OTHER_ATTR",
+          "EVENTS",
+          "CONTENT",
+        ],
+      },
+    ],
+  },
+};
